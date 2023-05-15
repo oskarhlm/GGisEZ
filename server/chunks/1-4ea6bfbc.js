3ff172134d3ea4c2381dfc50fc198291b35cd28b
Karl Oskar Magnus Holm <koholm@stud.ntnu.no>
2023-05-15 15:54:23 +0200

    Updates

--- server/chunks/1-4ea6bfbc.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-const component = async () => (await import('./error.svelte-8f36225d.js')).default;
-const file = '_app/immutable/entry/error.svelte.c865dba9.js';
-const imports = ["_app/immutable/entry/error.svelte.c865dba9.js","_app/immutable/chunks/index.d7db5ae4.js","_app/immutable/chunks/singletons.5501d12e.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, file, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-4ea6bfbc.js.map
--- server/chunks/layout.svelte-da4f8558.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from './index-5807c32b.js';
-
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `${slots.default ? slots.default({}) : ``}`;
-});
-
-export { Layout as default };
-//# sourceMappingURL=layout.svelte-da4f8558.js.map
