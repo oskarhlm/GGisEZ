48f073026f635323758fbf915079a8ded1944fc8
Karl Oskar Magnus Holm <koholm@stud.ntnu.no>
2023-05-23 16:33:20 +0200

    Updates

--- _app/immutable/chunks/1.573766f7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.8b13b316.js";export{t as component};
---
+++ _app/immutable/chunks/1.cb9072ab.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.1f3ef2f1.js";export{t as component};
